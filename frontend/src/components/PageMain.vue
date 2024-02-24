94c22ec0a89bc64ab80b71e8abfd9f422f38d034
Konstantin Kot <sigankov.06@mail.ru>
2024-02-24 23:28:00 +0500

    Created page "About"

--- frontend/src/components/PageMain.vue
+++ frontend/src/components/PageMain.vue
@@ -2,6 +2,9 @@
 import InformationSlide from "@/components/InformationSlide.vue";
 import InformationBlockRight from "@/components/InformationBlockRight.vue";
 import InformationBlockLeft from "@/components/InformationBlockLeft.vue";
+import arg1 from '@/assets/arg1.png?url'
+import arg2 from '@/assets/arg2.png?url'
+import arg3 from '@/assets/arg3.png?url'
 </script>
 
 <template>
@@ -13,18 +16,18 @@ import InformationBlockLeft from "@/components/InformationBlockLeft.vue";
     </h4>
     <section class="row gy-3 row-cols-1">
       <section class="col">
-      <InformationBlockRight img-src="/src/assets/arg1.png"
+      <InformationBlockRight :img-src="arg1"
                              desc="Овощи"
                              text="Мы используем только свежие и отборные продукты."/>
       </section>
       <section class="col">
-      <InformationBlockLeft img-src="/src/assets/arg2.png"
+      <InformationBlockLeft :img-src="arg2"
                             desc="блюдо"
                             text="Наши сотрудники - профессионалы своего дела.
                             Приготовить вкусное еду для вас - это наша приоритетная цель."/>
       </section>
       <section class="col">
-        <InformationBlockRight img-src="/src/assets/arg3.png"
+        <InformationBlockRight :img-src="arg3"
                                desc="Гитарист"
                                text="У нас вы можете заказать музыкантов,
                                чтобы приятно провести время."/>
